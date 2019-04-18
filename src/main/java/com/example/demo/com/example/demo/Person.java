ab9174dc345ced0ac47134c0a4a59f81390bdf49
agrim07 <dhankhar.amaresh@gmail.com>
2019-04-18 13:37:40 +0530

    First Commit

---
+++ src/main/java/com/example/demo/com/example/demo/Person.java
@@ -0,0 +1,18 @@
+package com.example.demo.com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
